382d82eb29745443605fe3014995144d18228436
tlun0911 <tlun0911@gmail.com>
2024-05-25 16:41:41 -0400

    Updated add meal page

--- frontend/src/pages/AddMealPage.jsx
+++ frontend/src/pages/AddMealPage.jsx
@@ -3,6 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux'
 import { toast } from 'react-toastify';
 import { createMeal }  from '../features/meals/mealSlice'
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import { FaPlus } from 'react-icons/fa6'
 import './AddMealPage.css';
 
 const AddMealPage = () => {
@@ -35,6 +40,15 @@ const AddMealPage = () => {
     const submitForm = (e) =>{
         e.preventDefault()
         dispatch(createMeal(mealData))
+        toast.success('Meal submitted successfully')
+        let clearMealData = {
+            meal_name: '',
+            type: '',
+            ingredients: ['', '', ''],
+            recipe: '',
+        }
+        setMealData(clearMealData)
+        navigate('/add-meal')
 
     };
 
@@ -57,105 +71,74 @@ const AddMealPage = () => {
 
 
   return (
-    <>
-        <section className="bg-primary py-5 mb-4">
-            <div className="container mx-auto py-5 custom-max-width">
-                <div className="bg-light px-4 py-4 mb-4 shadow rounded border m-4 m-md-0">
-                    <form onSubmit={submitForm}>
 
-                    <div className='mb-4'>
-                        <label className="d-block text-dark fw-bold mb-2">
-                            Meal Name
-                        <input 
-                            type='text'
-                            id='meal_name'
-                            name='meal_name'
-                            className="form-control mb-2 border-primary"
-                            required
-                            value={meal_name}
-                            onChange={onChange}
-                        />
-                        </label>
-                    </div>
-                    <div className='mb-4'>
-                        <label className='d-block text-dark fw-bold px-3'>
-                            Meal Type
-                            <div className="form-check form-check m-1 border-primary">
-                                <input className="form-check-input border-primary" type="radio"
-                                id="breakfast-radio"
-                                name="type"
-                                value="breakfast"
-                                onChange={onChange} />
-                                <label className="form-check-label" htmlFor="breakfast-radio">
-                                    Breakfast
-                                </label>
-                            </div>
-                            <div className="form-check form-check m-1">
-                                <input className="form-check-input border-primary" type="radio"
-                                id="lunch-radio"
-                                name="type"
-                                value="lunch"
-                                onChange={onChange} />
-                                <label className="form-check-label" htmlFor="lunch-radio">
-                                    Lunch
-                                </label>
-                            </div>
-                            <div className="form-check form-check m-1">
-                                <input className="form-check-input border-primary" type="radio"
-                                id="dinner-radio"
-                                name="type"
-                                value="dinner"
-                                onChange={onChange} />
-                                <label className="form-check-label" htmlFor="dinner-radio">
-                                    Dinner
-                                </label>
-                            </div>
-                        </label>
-                    </div>
-                    <div className='mb-4'>
-                        <label className="d-block text-dark fw-bold mb-2">
-                            Ingredients
-                        {mealData.ingredients.map((ingredient, index) => (
-                            <div key={index}>
-                                <div className="input-group mb-3">
-                                    <span className="input-group-text border-primary">{`Ingredient ${index+1}`}</span>
-                                    <input
-                                        type='text'
-                                        id={index}
-                                        name='ingredient'
-                                        className="form-control border-primary"
-                                        required
-                                        value={ingredient}
-                                        onChange={(e) => handleIngredientChange(index, e)}
-                                    />
-                                </div>
-                            </div>
-                        ))}
-                        </label>
-                        
-                        <button type="button" className="btn btn-primary" onClick={addInputField}>Add Additional Ingredient</button> 
-                    </div>
+    <Form 
+        onSubmit={submitForm}
+        className="container mx-auto mt-3 bg-light shadow rounded border">
 
-                    <div className="mb-3">
-                        <label htmlFor="recipe-input" className="form-label fw-bold">Recipe Input</label>
-                        <textarea className="form-control border-primary"
-                                    id="recipe-input" 
-                                    name="recipe"
-                                    rows="3"
-                                    value={recipe}
-                                    onChange={onChange}
-                                    />
-                    </div>
+        <Form.Group className="mb-3 border-primary">
+            <Form.Label className='h4 my-2'>Meal Name</Form.Label>
+            <Form.Control type="text" 
+            placeholder="Enter meal name"
+            className='border-primary'
+            name="meal_name"
+            value={meal_name}
+            required 
+            onChange={onChange}/>
+        </Form.Group>
 
-                    <div className='d-flex justify-content-center'>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </div>
+        <Form.Select aria-label="Default select example" 
+            id="type"
+            className='border-primary mb-3'
+            required
+            name="type"
+            onChange={onChange}>
+            <option>Select Meal Type</option>
+            <option value="breakfast">Breakfast</option>
+            <option value="lunch">Lunch</option>
+            <option value="dinner">Dinner</option>
+        </Form.Select>
 
-                    </form>
-                </div>
-            </div>
-        </section>
-    </>
+        {mealData.ingredients.map((ingredient, index) => (
+            <InputGroup className="mb-3" key={index}>
+                <InputGroup.Text id="inputGroup-sizing-default">
+                {`Ingredient ${index+1}`}
+                </InputGroup.Text>
+                <Form.Control
+                aria-label="Default"
+                aria-describedby="inputGroup-sizing-default"
+                className='border-primary'
+                type='text'
+                required
+                id={index}
+                name='ingredient'
+                value={ingredient}
+                onChange={(e) => handleIngredientChange(index, e)}
+                />
+            </InputGroup>
+            ))}
+
+        <Button variant="outline-primary" className='mb-3' onClick={addInputField}>
+            <FaPlus />
+        </Button>
+
+        <InputGroup className='border-primary mb-3'>
+            <InputGroup.Text>Enter Recipe:</InputGroup.Text>
+            <Form.Control as="textarea"
+            required
+            id="recipe"
+            value={recipe}
+            rows='4'
+            name="recipe"
+            onChange={onChange} 
+            aria-label="With textarea" />
+        </InputGroup>
+        <Container className='d-flex justify-content-center mb-3'>
+            <Button variant="primary" className='' type="submit" value="submit">Submit</Button>
+        </Container>
+    </Form>
+    
+
   )
 }
 
